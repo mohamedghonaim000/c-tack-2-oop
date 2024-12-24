d258027ea0f2e43a6025de165bcd05b99fa1c4a6
mohamedghonaim000 <mohamedmokhtar64232@gmail.com>
2024-12-24 10:51:19 +0200

    initial commit

---
+++ Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace oop_assigment_02
+{
+    enum securityprivileges
+    {
+        guest, Developer, secretary , DBA
+    }
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private securityprivileges Securitylevel { get; set; }
+        public int Salary { get; set; }
+        public HiringDate HiringDate { get; set; }
+        private char Gender; 
+        public char _gender {
+            get { return Gender;  }
+            
+            set {
+                if (value == 'M' || value == 'F')
+                    Gender = value;
+                else { Console.WriteLine("enter a valid gender"); }
+            } 
+        }
+
+        public Employee(int id, string name,  int salary,  char gender)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+            Gender = gender;
+        }
+        public void Assignsecuritylevel(securityprivileges input) {
+            Securitylevel = input;  
+        }
+
+        public override string ToString()
+        {
+            return $"id : {Id}  name : {Name}  slary : {Salary} Gender : {Gender} securitylevel : {Securitylevel} ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace oop_assigment_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(66, "mohamed", 4555, 'M');
+            e1.Assignsecuritylevel(securityprivileges.Developer);
+            e1.HiringDate = new HiringDate(12, 12, 2024);
+            Console.WriteLine(e1.ToString());
+
+            Employee[] emp = new Employee[] {
+               new Employee(1, "mohamed", 6000, 'F'),
+               new Employee(2, "ahmed", 4000, 'M'),
+               new Employee(3, "ali", 7000, 'M'),
+            };
+
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine(emp[i]);
+            }
+
+        }
+    }
+}
